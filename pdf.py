ca872d345db751c641125f3a07aee211c729974c
zenyaboi <bannedboyinlife@gmail.com>
2025-02-28 13:22:01 -0300

    added more seller info for testing

--- pdf.py
+++ pdf.py
@@ -4,7 +4,7 @@ from reportlab.pdfgen import canvas
 def add_background(canvas, image_path):
     canvas.drawImage(image_path, 0, 0, width=595, height=842)
 
-def add_overlay_text(canvas, x, y, text, font_size=12, font_name="Helvetica-Bold"):
+def add_overlay_text(canvas, x, y, text, font_size, font_name="Helvetica-Bold"):
     canvas.setFont(font_name, font_size)
     canvas.setFillColorRGB(0, 0, 0)
     canvas.drawString(x, y, text)
@@ -20,7 +20,15 @@ def createPDF(filename):
 
     # Page 1
     add_background(c, image_page1)
-    add_overlay_text(c, 110, 615, "TESTE TESTE TESTE TESTE TESTE!")
+    # contract info
+    add_overlay_text(c, 130, 651, "000/SB/0000", 10)
+    add_overlay_text(c, 324, 651, "00 de Fevereiro de 0000", 10)
+    # seller info
+    add_overlay_text(c, 110, 615, "TESTE TESTE TESTE TESTE TESTE!", 12)
+    add_overlay_text(c, 138, 597, "12.123.123/0001-23", 10)
+    add_overlay_text(c, 280, 597, "TESTE TESTE TESTE!", 10)
+    add_overlay_text(c, 155, 579, "RUA TESTE TESTE TESTE TESTE TESTE, 202, NUMERO CASA TERREO AP RODOVIA", 10)
+
     c.showPage()  # Finalize the current page and start a new one
 
     # Page 2
